03d74a7535f288c513c5494009cabf29fddf5e99
ndanhtu <nguyendoanhtu14@gmail.com>
2026-04-08 13:15:43 +0700

    add pagination

--- controllers/index.php
+++ controllers/index.php
@@ -3,9 +3,23 @@
 use Core\Database;
 
 $db = new Database();
+$page = $_GET['page'] ?? 1;
+$product_per_page = 6;
+$offset = ($page - 1) * $product_per_page;
+$numberOfProducts = $db->query("SELECT COUNT(id) as count FROM products")->getOnce();
+$pages = ceil($numberOfProducts['count'] / $product_per_page);
 
-$data = $db->query('SELECT p.id, p.name, p.price, p.short_description, p.description, p.image, c.name as category_name FROM products as p JOIN categories as c ON p.category = c.id')->getAll();
+$data = $db->query("SELECT p.id, p.name, p.price, p.short_description, p.description, p.image, c.name as category_name 
+FROM products as p 
+JOIN categories as c 
+ON p.category = c.id 
+ORDER BY p.id
+LIMIT $product_per_page 
+OFFSET $offset")->getAll();
 
 view('index.view.php', [
     'products' => $data,
+    'numberOfProducts' => $numberOfProducts,
+    'pages' => $pages,
+    'page' => $page
 ]);
--- views/index.view.php
+++ views/index.view.php
@@ -7,6 +7,13 @@
         <p>Khám phá bộ sưu tập Gundam, Figure, Lego mới nhất!</p>
     </div>
     <p class="main-title">SẢN PHẨM MỚI</p>
+    <div class="pages">
+        <?php for ($i = 1; $i <= $pages; $i++) : ?>
+            <a href="/?page=<?= $i ?>" class="<?= $i == $page ? 'page-active' : '' ?>">
+                <?= $i ?>
+            </a>
+        <?php endfor; ?>
+    </div>
     <div class="items">
         <?php foreach ($products as $product) : ?>
             <a href="/detail?id=<?= $product['id'] ?>">
